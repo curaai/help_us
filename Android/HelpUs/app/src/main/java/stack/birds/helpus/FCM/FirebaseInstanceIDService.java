74ca891899d8ead595cf04a7bb95ce18aa548345
zhsir123 <zhsir123@gmail.com>
2017-07-25 20:26:46 +0900

    refresh error fixed but...

--- Android/HelpUs/app/src/main/java/stack/birds/helpus/FCM/FirebaseInstanceIDService.java
+++ Android/HelpUs/app/src/main/java/stack/birds/helpus/FCM/FirebaseInstanceIDService.java
@@ -2,8 +2,13 @@ package stack.birds.helpus.FCM;
 
 import android.util.Log;
 
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.util.HashMap;
 
 /**
  * Created by dsm2016 on 2017-07-24.
@@ -11,6 +16,8 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
 
 public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
     private static final String TAG = "MyFirebaseIIDService";
+    private String HOST_URL = "https://dmlwlsdk07.000webhostapp.com/register.php";
+    private AQuery aq;
 
     // 프로그램이 처음 생길시 토큰 생성
     @Override
@@ -23,6 +30,20 @@ public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
 
     private void sendRegistrationToServer(String token){
         // 서버로 토큰 보내기
+        HashMap<String, String> param = new HashMap<String, String>();
+        param.put("registration_ids", token);
+
+        aq = new AQuery(getApplicationContext());
+        aq.ajax(HOST_URL, param, String.class, new AjaxCallback<String>() {
+            @Override
+            public void callback(String url, String object, AjaxStatus status) {
+                if(status.getCode() == 200) {
+                    Log.d(TAG, "SUCCESS");
+                } else {
+                    Log.d(TAG, "FAILED");
+                }
+            }
+        });
     }
 
 }
--- Android/HelpUs/app/src/main/java/stack/birds/helpus/MainActivity.java
+++ Android/HelpUs/app/src/main/java/stack/birds/helpus/MainActivity.java
@@ -1,19 +1,34 @@
 package stack.birds.helpus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text1;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String token = FirebaseInstanceId.getInstance().getToken();
-        Log.d("FCM_Token", token);
+        text1 = (TextView) findViewById(R.id.text_result);
+        btn1 = (Button) findViewById(R.id.btn_refresh);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String token = FirebaseInstanceId.getInstance().getToken();
+                text1.setText(token);
+                Log.d("token", token);
+            }
+        });
     }
 }
