0bedf9c6ff368359a240c9744c4a501877dfca58
zhsir123 <zhsir123@gmail.com>
2017-07-26 12:46:42 +0900

    remove wrong directory

--- Android/HelpUs/app/src/main/java/stack/birds/helpus/MainActivity.java
+++ Android/HelpUs/app/src/main/java/stack/birds/helpus/MainActivity.java
@@ -1,32 +1,53 @@
 package stack.birds.helpus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import stack.birds.helpus.FCM.FirebaseInstanceIDService;
+import stack.birds.helpus.GPS.GPSService;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text1;
-    Button btn1;
+    TextView langitude, longitude;
+    Button refresh, gps_btn;
 
-    ;
+    GPSService gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text1 = (TextView) findViewById(R.id.text_result);
-        btn1 = (Button) findViewById(R.id.btn_refresh);
+        langitude = (TextView) findViewById(R.id.latitude);
+        longitude = (TextView) findViewById(R.id.longitude);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        refresh = (Button) findViewById(R.id.btn_refresh);
+        gps_btn = (Button) findViewById(R.id.gps_on);
+
+        refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseInstanceIDService fb = new FirebaseInstanceIDService(getApplicationContext());
+            }
+        });
+
+        gps_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gps = new GPSService(getApplicationContext());
+
+                gps.listening_gps();
+                Log.d("gps in activity", gps.getLat() + "");
+                Log.d("gps in activity", gps.getLon() + "");
+
+                langitude.setText(Double.toString(gps.getLat()));
+                longitude.setText(Double.toString(gps.getLon()));
             }
         });
 
