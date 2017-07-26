876e6f770631e36f7917487cfe26c4eb48244f59
zhsir123 <zhsir123@gmail.com>
2017-07-26 16:15:30 +0900

    startactivity in listener

--- Android/HelpUs/app/src/main/java/stack/birds/helpus/AccountActivity/LandingActivity.java
+++ Android/HelpUs/app/src/main/java/stack/birds/helpus/AccountActivity/LandingActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -36,22 +37,22 @@ public class LandingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
             }
         });
         regist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 intent = new Intent(context, SignupActivity.class);
+                startActivity(intent);
             }
         });
         forget_pw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 intent = new Intent(context, ForgetAccountActivity.class);
+                startActivity(intent);
             }
         });
-
-        startActivity(intent);
-        finish();
     }
 }
