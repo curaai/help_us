e063714d0c9b0709c2a7aaafc6bac0635ede8411
leejaebin <dmlwlsdk07@gmail.com>
2017-07-28 16:01:18 +0900

    login and join complete

--- Server/joinin.php
+++ Server/joinin.php
@@ -2,18 +2,21 @@
   $id = $_POST['id'];
   $pw = $_POST['pw'];
   $name = $_POST['name'];
-  include './config.php';
-  $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+  $log  = "ID: ".$id.PHP_EOL.
+        "PW: ".$pw.PHP_EOL.
+        "NAME: ".$name.PHP_EOL.
+        "-------------------------".PHP_EOL;
+  file_put_contents('/public_html/log.txt', $log, FILE_APPEND);
+  $conn = mysqli_connect('localhost', 'id2349841_root', 'santajen05', 'id2349841_fcm');
   $sql = "SELECT id FROM users";
   $result = mysqli_query($conn, $sql);
-  while($row = mysqli_fetch_assoc($result){
-    if($ow['id'] == $id){
-      header("Result: Already Exists!");
+  while($row = mysqli_fetch_assoc($result)){
+    if($row['id'] == $id){
+      header("Result:0");
       exit;
     }
   }
-  $query = "INSERT INTO users(id, pw, name) Values ('$id', '$pw', '$name');";
-	mysqli_query($conn, $query);
-  header("Result: Success!");
-  }
+  $query = "INSERT INTO users(id, pw, name) VALUES ('$id', '$pw', '$name')";
+  mysqli_query($conn, $query);
+  header("Result:1");
 ?>
--- Server/login_procs.php
+++ Server/login_procs.php
@@ -1,14 +1,17 @@
 <?php
   $id = $_POST['id'];
   $pw = $_POST['pw'];
+  $token = $_POST['token'];
   include './config.php';
   $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
   $query = "SELECT id, pw FROM users WHERE id = '$id' AND pw = '$pw'";
   $result = mysqli_query($conn, $query);
   $row = mysqli_num_rows($result);
   if($row > 0){
-      header("Result: Success");
+      header("Result: 1");
   } else{
-    header("Result: Fail");
+    header("Result: 0");
   }
+  $query = "UPDATE users SET Token = '$token' WHERE id= '$id'";
+  $result = mysqli_query($conn, $query);
 ?>
